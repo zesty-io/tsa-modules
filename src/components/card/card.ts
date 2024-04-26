974493c61a0ed9faf6fefb3fa6e9cee04074c84b
kharle14 <enriquezkharl14@gmail.com>
2024-04-26 23:14:54 +0800

    Update card component to be class base

--- src/components/card/card.ts
+++ src/components/card/card.ts
@@ -1,41 +1,13 @@
 import '../card/card.css';
 
-const styles = new CSSStyleSheet();
-
-styles.replaceSync(`
-  :host{
-    padding: 3.2rem 1.6rem;
-    box-shadow: 0px 8px 35px 0px rgba(0, 0, 0, 0.25);
-    background: rgba(239, 241, 240, 1);
-    display: inline-block;
-  }
-`);
-
-export class TSACardComponent extends HTMLElement {
-  constructor() {
-    super();
-    this.attachShadow({ mode: 'open' });
-  }
-
-  connectedCallback() {
-    if (this.shadowRoot) {
-      this.shadowRoot.adoptedStyleSheets = [styles];
-
-      this.shadowRoot.innerHTML = `
-          <slot></slot>
-      `;
-    }
-  }
-}
-
 export interface CardProps {
   content: string;
 }
 
 export const Card = ({ content }: CardProps) => {
   return `
-      <tsa-card>
+      <div class="tsa-card">
         ${content}
-      </tsa-card>
+      </div>
     `;
 };
