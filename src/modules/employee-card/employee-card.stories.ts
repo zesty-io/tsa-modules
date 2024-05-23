7f9fcf6b5fe209e0d3ba93f07bb77d4133bf6fcc
kharle14 <enriquezkharl14@gmail.com>
2024-05-23 18:37:29 +0800

    [Feat] Employee Card

---
+++ src/modules/employee-card/employee-card.stories.ts
@@ -0,0 +1,17 @@
+import { Meta, StoryObj } from '@storybook/html';
+import { EmployeeCard } from './employee-card';
+
+const meta = {
+  title: 'Module/Employee Card',
+  parameters: {
+    layout: 'centered'
+  },
+  render: () => EmployeeCard(),
+  tags: ['autodocs']
+} satisfies Meta;
+
+export default meta;
+
+export const Example: StoryObj = {
+  args: {}
+};
---
+++ src/modules/employee-card/employee-card.ts
@@ -0,0 +1,11 @@
+import './employee-card.css';
+
+export const EmployeeCard = () => {
+  return `
+    <div class="tsa-card employee-card">
+        <img src="https://w1w702z1.media.zestyio.com/Frame-440.png" alt="John Doe">
+        <h1>[FPO] “Working at The Salvation Army is so fulfilling”</h1>
+        <span>- Jane<span>
+    </div>
+  `;
+};
