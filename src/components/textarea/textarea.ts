a9259481dc9742599f07a39a1bc8020960151229
Kharl <enriquezkharl14@gmail.com>
2024-07-22 20:38:10 +0800

    [Style] Update font sizing - Text area component - Input component - Text with icon component

--- src/components/textarea/textarea.ts
+++ src/components/textarea/textarea.ts
@@ -1,28 +1,28 @@
-import './textarea.css';
+import "./textarea.css";
 
 export interface TextareaProps {
-  name: string;
-  size?: 'small' | 'medium' | 'large';
-  type: 'email' | 'text' | 'password' | 'number';
-  value: string;
-  placeholder: string;
-  disabled?: boolean;
-  label?: string;
+	name: string;
+	size?: "small" | "medium" | "large";
+	type: "email" | "text" | "password" | "number";
+	value: string;
+	placeholder: string;
+	disabled?: boolean;
+	label?: string;
 }
 
 export const Textarea = ({
-  name,
-  size = 'medium',
-  type = 'text',
-  value = '',
-  placeholder,
-  disabled = false,
-  label
+	name,
+	size = "medium",
+	type = "text",
+	value = "",
+	placeholder,
+	disabled = false,
+	label,
 }: TextareaProps) => {
-  return `
+	return `
     <label class="textarea">
-        <span class="placeholder">${label}</span>
-        <textarea class="textarea--${size}" name="${name}" placeholder="${placeholder}" type="${type}" ${disabled && 'disabled'}>${value}</textarea>
+        <span>${label}</span>
+        <textarea class="textarea--${size}" name="${name}" placeholder="${placeholder}" type="${type}" ${disabled && "disabled"}>${value}</textarea>
     </label>
   `;
 };
