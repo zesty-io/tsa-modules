7b1b8396be575f7928d899f2a22505f98d87506b
Kharl <enriquezkharl14@gmail.com>
2024-07-22 21:04:39 +0800

    [Refactor] Update Hero component - Update CTA hide and show - Text sizing - Margin / padding

--- src/features/common/header/header-module.stories.ts
+++ src/features/common/header/header-module.stories.ts
@@ -1,10 +1,10 @@
-import { Meta, StoryObj } from '@storybook/html';
-import { Header, HeaderProps } from './header-module';
+import type { Meta, StoryObj } from "@storybook/html";
+import { Header, type HeaderProps } from "./header-module";
 
 const meta = {
-  title: 'Features/Common/Header',
-  tags: ['autodocs'],
-  render: (args) => Header(args)
+	title: "Features/Common/Header",
+	tags: ["autodocs"],
+	render: (args) => Header(args),
 } satisfies Meta<HeaderProps>;
 
 export default meta;
@@ -12,9 +12,10 @@ export default meta;
 type Story = StoryObj<HeaderProps>;
 
 export const Example: Story = {
-  args: {
-    src: 'https://w1w702z1.media.zestyio.com/image-1.png',
-    srcLarge: 'https://w1w702z1.media.zestyio.com/image-2.png',
-    href: 'http://localhost:6006/?path=/docs/modules-header--docs'
-  }
+	args: {
+		src: "https://w1w702z1.media.zestyio.com/image-1.png",
+		srcLarge: "https://w1w702z1.media.zestyio.com/image-2.png",
+		href: "http://localhost:6006/?path=/docs/modules-header--docs",
+		singleCTA: true,
+	},
 };
--- src/features/common/header/header-module.ts
+++ src/features/common/header/header-module.ts
@@ -1,17 +1,13 @@
-import './header-module.scss';
+import "./header-module.scss";
 
 export class HeaderModule extends HTMLElement {
-  constructor() {
-    super();
-  }
-
-  connectedCallback() {
-    this.innerHTML = `
+	connectedCallback() {
+		this.innerHTML = `
     <div class="tsa-header">
         <div class="tsa-header__logo">
-            <a href="${this.getAttribute('href') ?? '/'}">
-                <img class="logo-small" src="${this.getAttribute('src')}" alt="Salvation Army Logo"/>
-                <img class="logo-large" src="${this.getAttribute('srcLarge')}" alt="Salvation Army Logo"/>
+            <a href="${this.getAttribute("href") ?? "/"}">
+                <img class="logo-small" src="${this.getAttribute("src")}" alt="Salvation Army Logo"/>
+                <img class="logo-large" src="${this.getAttribute("srcLarge")}" alt="Salvation Army Logo"/>
             </a>
             <tsa-switch labels="EN,ES"></tsa-switch>
         </div>
@@ -19,13 +15,17 @@ export class HeaderModule extends HTMLElement {
         <div class="tsa-header__input">
             <label class="input">
                 <i class="bi bi-search"></i>
-                <input class="input--medium" name="" value="" placeholder="What are you looking for" type="text"/>
+                <input class="input--small" name="" value="" placeholder="What are you looking for" type="text"/>
             </label>
         </div>
 
         <div class="tsa-header__cta">
-            <button type="button" class="btn btn--small btn--dark--fill ">Find Help</button>
-            <button type="button" class="btn btn--small btn--dark--fill ">Donate</button>
+          ${
+						this.getAttribute("data-single-cta")
+							? ""
+							: '<button type="button" class="me-5 btn btn-dark">Find Help</button>'
+					}
+            <button type="button" class="btn btn-dark">Donate</button>
         </div>
 
         <button class="tsa-header__burger">
@@ -33,17 +33,18 @@ export class HeaderModule extends HTMLElement {
         </button>
     </div>
     `;
-  }
+	}
 }
 
 export interface HeaderProps {
-  src: String;
-  href: String;
-  srcLarge: String;
+	src: string;
+	href: string;
+	srcLarge: string;
+	singleCTA: boolean;
 }
 
-export const Header = ({ src, href, srcLarge }: HeaderProps) => {
-  return `
-    <header-module srcLarge="${srcLarge}" src="${src}" href="${href}"></header-module>
+export const Header = ({ src, href, srcLarge, singleCTA }: HeaderProps) => {
+	return `
+    <header-module ${singleCTA ? 'data-single-cta="true"' : ""} srcLarge="${srcLarge}" src="${src}" href="${href}"></header-module>
     `;
 };
