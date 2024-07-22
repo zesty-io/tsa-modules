4265ac86fee224158f49bb7e0295ceda4217ff0b
Kharl <enriquezkharl14@gmail.com>
2024-07-22 20:29:48 +0800

    [Refactor] Update Switch - Text sizing - Width and height

--- src/components/switch/switch.ts
+++ src/components/switch/switch.ts
@@ -1,47 +1,43 @@
-import './switch.css';
+import "./switch.css";
 
 export class SwitchComponent extends HTMLElement {
-  constructor() {
-    super();
-  }
+	connectedCallback() {
+		const labels = this.getAttribute("labels");
 
-  connectedCallback() {
-    const labels = this.getAttribute('labels');
+		const labelsArr = labels?.split(",") ?? [];
 
-    const labelsArr = labels?.split(',') ?? [];
-
-    this.innerHTML = `
+		this.innerHTML = `
         <button class="tsa-switch">
-            <div class="tsa-switch__thumb">${labelsArr[0] ?? ''}</div>
+            <div class="tsa-switch__thumb">${labelsArr[0] ?? ""}</div>
             <div class="tsa-switch__label">
-                <span>${labelsArr[0] ?? ''}</span>
-                <span>${labelsArr[1] ?? ''}</span>
+                <span>${labelsArr[0] ?? ""}</span>
+                <span>${labelsArr[1] ?? ""}</span>
             </div>
             <input type="checkbox">
         </button>
 
     `;
 
-    const toggleButton = this.querySelector('.tsa-switch');
+		const toggleButton = this.querySelector(".tsa-switch");
 
-    const toggleThumb = this.querySelector('.tsa-switch__thumb');
+		const toggleThumb = this.querySelector(".tsa-switch__thumb");
 
-    toggleButton?.addEventListener('click', () => {
-      toggleButton.toggleAttribute('toggled');
+		toggleButton?.addEventListener("click", () => {
+			toggleButton.toggleAttribute("toggled");
 
-      if (toggleButton.hasAttribute('toggled')) {
-        if (toggleThumb) toggleThumb.textContent = labelsArr[1];
-      } else {
-        if (toggleThumb) toggleThumb.textContent = labelsArr[0];
-      }
-    });
-  }
+			if (toggleButton.hasAttribute("toggled")) {
+				if (toggleThumb) toggleThumb.textContent = labelsArr[1];
+			} else {
+				if (toggleThumb) toggleThumb.textContent = labelsArr[0];
+			}
+		});
+	}
 }
 
 export interface SwitchProps {
-  labels: String;
+	labels: string;
 }
 
 export const Switch = ({ labels }: SwitchProps) => {
-  return `<tsa-switch labels="${labels}"></tsa-switch>`;
+	return `<tsa-switch labels="${labels}"></tsa-switch>`;
 };
