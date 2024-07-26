4c1982a48e434024b13c3e701cf35ee2f734cf3d
Kharl <enriquezkharl14@gmail.com>
2024-07-27 00:06:39 +0800

    [Feat] Footer mobile

--- src/features/common/footer/footer.stories.ts
+++ src/features/common/footer/footer.stories.ts
@@ -1,14 +1,86 @@
 import type { Meta, StoryObj } from "@storybook/html";
-import { Footer } from "./footer";
+import { Footer, type FooterProps } from "./footer";
 
 const meta = {
 	title: "Features/Common/Footer",
-	render: () => Footer(),
+	parameters: {
+		layout: "fullscreen",
+	},
+	render: (args) => Footer(args),
 	tags: ["autodocs"],
-} satisfies Meta;
+} satisfies Meta<FooterProps>;
 
 export default meta;
 
-export const Example: StoryObj = {
-	args: {},
+type Story = StoryObj<FooterProps>;
+
+export const Example: Story = {
+	args: {
+		statement:
+			"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididun ut labore et dolore magna aliqua. Ut",
+		socialLinks: [
+			{
+				icon: "https://mgfrnv8q.media.zestyio.com/ic_baseline-facebook.png",
+				url: "https://www.facebook.com",
+			},
+			{
+				icon: "https://mgfrnv8q.media.zestyio.com/mdi_twitter.png",
+				url: "https://www.x.com",
+			},
+			{
+				icon: "https://mgfrnv8q.media.zestyio.com/mdi_youtube.png",
+				url: "https://www.youtube.com",
+			},
+			{
+				icon: "https://mgfrnv8q.media.zestyio.com/mdi_instagram.png",
+				url: "https://www.instagram.com",
+			},
+			{
+				icon: "https://mgfrnv8q.media.zestyio.com/mdi_linkedin.png",
+				url: "https://www.linkedin.com",
+			},
+		],
+		navigationLinks: [
+			{
+				url: "#",
+				name: "Home",
+			},
+			{
+				url: "#",
+				name: "About Us",
+			},
+			{
+				url: "#",
+				name: "Locations",
+			},
+			{
+				url: "#",
+				name: "Services",
+			},
+			{
+				url: "#",
+				name: "Newsroom",
+			},
+			{
+				url: "#",
+				name: "Ways To Give",
+			},
+			{
+				url: "#",
+				name: "Employment Opportunities",
+			},
+			{
+				url: "#",
+				name: "Volunteer",
+			},
+			{
+				url: "#",
+				name: "Accessibility",
+			},
+			{
+				url: "#",
+				name: "Affiliate Links",
+			},
+		],
+	},
 };
--- src/features/common/footer/footer.ts
+++ src/features/common/footer/footer.ts
@@ -1,41 +1,121 @@
+// Todo: Handle sublinks in navigation links
+
 export class FooterModule extends HTMLElement {
 	connectedCallback() {
+		const socialLinks = this.querySelectorAll("tsa-social-link");
+		const navigationLinks = this.querySelectorAll(".navigation-link");
+
 		this.innerHTML = `
-		<div>
-			<div>
-				<h1>Salvation Army Mission Statement</h1>
-				<p>${this.getAttribute("data-statement")}</p>
+		<div id="footer-module" class="py-6 px-3">
+			<div class="d-flex text-center justify-content-center flex-column gap-2">
+				<h3 class="fs-5">Salvation Army Mission Statement</h1>
+				<p class="text-center lh-base">${this.getAttribute("data-statement")}</p>
 			</div>
 
-			<div>
-				
+			<hr />
+
+			<div id="footer-navigation-links-container" class="d-flex flex-column gap-3"></div>
+
+			<div class="my-5">
+				<tsa-header-search></tsa-header-search>
 			</div>
 
-			<div>
-		    	<tsa-header-search></tsa-header-search>
-			</div>
+			<div id="footer-social-links-container" class="d-flex justify-content-evenly"></div>
 
-			<div id="footer-social-links-container">		
+			<div class="d-flex justify-content-evenly my-8">
+				<div>
+					<img
+					src="https://mgfrnv8q.media.zestyio.com/image-3.png"
+					alt="Location icon"
+					class="me-1"
+					/>
+				</div>
+				<div>
+					<img
+					src="https://mgfrnv8q.media.zestyio.com/image-4.png"
+					alt="Location icon"
+					class="me-1"
+					/>
+				</div>
+				<div>
+					<img
+					src="https://mgfrnv8q.media.zestyio.com/image-5.png"
+					alt="Location icon"
+					class="me-1"
+					/>
+				</div>
+    		</div>
+
+			<hr />
+
+
+			<div class="d-flex justify-content-between align-items-center">
+				<div class="footer-policy text-black text-center">
+				<p class="my-4 fs-7">The Salvation Army USA</p>
+				<p class="fs-7 text-wrap">
+					615 Slaters Lane, Alexandria, Virginia 22314 | 1-800-SAL-ARMY |
+					<a
+					class="link-dark"
+					href=""
+					>Privacy Policy</a
+					>
+					| © The Salvation Army National Headquarters
+				</p>
+				</div>
 			</div>
 		</div>
+
 		`;
 
-		const socialLinksContainer = this.querySelector(
+		const footerModule = this.querySelector("#footer-module") as HTMLElement;
+		// This handles putting the social llinks to the container
+		const socialLinksContainer = footerModule?.querySelector(
 			"#footer-social-links-container",
 		);
-
-		const socialLinks = this.querySelectorAll("tsa-social-links");
-
 		for (let i = 0; i < socialLinks.length; i++) {
 			socialLinksContainer?.appendChild(socialLinks[i]);
+		}
+
+		const navigationLinksContainer = footerModule?.querySelector(
+			"#footer-navigation-links-container",
+		);
+		for (let i = 0; i < navigationLinks.length; i++) {
+			navigationLinksContainer?.appendChild(navigationLinks[i]);
 		}
 	}
 }
 
-export const Footer = () => {
+export type SocialLink = { url: string; icon: string };
+export type NavigationLink = { url: string; name: string };
+
+export interface FooterProps {
+	statement: string;
+	socialLinks: SocialLink[];
+	navigationLinks: NavigationLink[];
+}
+
+export const Footer = ({
+	statement,
+	socialLinks,
+	navigationLinks,
+}: FooterProps) => {
 	return `
-		<tsa-footer>
-			<tsa-social-link data-icon="https://mgfrnv8q.media.zestyio.com/ic_baseline-facebook.png" data-url="https://www.facebook.com"></tsa-social-link>
-		</tsa-footer>
-    `;
+  <tsa-footer data-statement="${statement}">
+    ${socialLinks
+			?.map(
+				({ url, icon }) => `
+      <tsa-social-link data-icon="${icon}" data-url="${url}"></tsa-social-link>
+    `,
+			)
+			.join("")}
+
+    ${navigationLinks
+			?.map(
+				({ url, name }) => `
+				<a class="navigation-link link-underline-opacity-0 link-dark" href="${url}">${name}</a>
+    `,
+			)
+			.join("")}
+  </tsa-footer>
+`;
 };
