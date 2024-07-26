682997d77a732fe9ab358a7c71c1e292293a2902
Kharl <enriquezkharl14@gmail.com>
2024-07-26 22:00:02 +0800

    [Feat] Update footer to render social links

--- src/features/common/footer/footer.ts
+++ src/features/common/footer/footer.ts
@@ -15,16 +15,27 @@ export class FooterModule extends HTMLElement {
 		    	<tsa-header-search></tsa-header-search>
 			</div>
 
-			<div>
-			
+			<div id="footer-social-links-container">		
 			</div>
 		</div>
 		`;
+
+		const socialLinksContainer = this.querySelector(
+			"#footer-social-links-container",
+		);
+
+		const socialLinks = this.querySelectorAll("tsa-social-links");
+
+		for (let i = 0; i < socialLinks.length; i++) {
+			socialLinksContainer?.appendChild(socialLinks[i]);
+		}
 	}
 }
 
 export const Footer = () => {
 	return `
-		<tsa-footer></tsa-footer>
+		<tsa-footer>
+			<tsa-social-link data-icon="https://mgfrnv8q.media.zestyio.com/ic_baseline-facebook.png" data-url="https://www.facebook.com"></tsa-social-link>
+		</tsa-footer>
     `;
 };
